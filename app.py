416a68e9a1cd869c835410cc8f14e1e2940a9d48
MarcoFRB <marcofrodriguez@hotmail.es>
2025-11-11 19:32:45 -0600

    UpdateV2_G5 app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ except FileNotFoundError:
     st.stop()
 
 # Título y subtítulo
-st.title("🤖 este es un nuevo titulo")
+st.title("🤖Predictor Grupo #5, Estrategia de BA")
 st.markdown("Esta app demuestra cómo el 'AI Factory' (Hugging Face) despliega un modelo de ML con una UI de Streamlit, todo disparado por GitOps.")
 
 # --- UI de Entrada (Sliders) ---
